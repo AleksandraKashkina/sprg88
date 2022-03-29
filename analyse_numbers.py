8351afbbe921befc06bdcfa6c0edd1aba6fbf575
Aleksandra Kashkina <226394@vutbr.cz>
2022-03-29 09:40:14 +0200

    1

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,10 +1,11 @@
 
 def is_whole_number(number):
-       if number % 1 == 0:
+       if (number % 1 == 0) and (number > 0):
            return True
 
 
        return False
 
-number = 22
-print(is_whole_number(number))
+my_numbers = [1, 2, 3]
+for my_number in my_numbers:
+    print(is_whole_number(number))
